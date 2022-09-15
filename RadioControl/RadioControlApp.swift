7c39ffd51be99fa70a00c56d3a89943f5bf4c88d
fayxos <felixhaag@hotmail.de>
2022-09-15 15:51:21 +0200

    Initial Commit

---
+++ RadioControl/RadioControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioControlApp.swift
+//  RadioControl
+//
+//  Created by Felix Haag on 15.09.22.
+//
+
+import SwiftUI
+
+@main
+struct RadioControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
